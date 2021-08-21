7f9b0af625829780abc5469c00b5753daad1c858
cachuelasaimon <cachuelasaimon@gmail.com>
2021-08-21 13:06:52 +0800

    BACKEND: Setup Mongo Connection

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import morgan from 'morgan'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-// MIDDLEWARES 
-let app = express()
-app.use(express.json())
-app.use(morgan('dev'))
+// MIDDLEWARES
+let app = express();
 
-// export app
-export default app
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+export default app;
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,32 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import app from "./app";
 
 dotenv.config({ path: "./config.env" });
 
-let port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server Started on Port: ${port}`));
+// Connection To Mongo
+let DATABASE: string | undefined = process.env.DATABASE as string;
+let PASSWORD: string | undefined = process.env.DATABASE_PASSWORD as string;
+let ConnectionString: string = DATABASE.replace("<PASSWORD>", PASSWORD);
+
+let ConnectToMongo = async (ConString: string) => {
+  try {
+    await mongoose.connect(ConString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    });
+    console.log(`Connect to Mongo`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+(async () => {
+  await ConnectToMongo(ConnectionString);
+
+  // Initialize Server
+  let port = process.env.PORT || 8000;
+  app.listen(port, () => console.log(`Server Started on Port: ${port}`));
+})();
